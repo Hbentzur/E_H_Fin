e154f2d3d06210c75779a28f07aefcdfe468140a
Hbentzur <31830472+Hbentzur@users.noreply.github.com>
2018-04-23 16:42:52 -0400

    Streaming works, how can we display it to canvas?

--- Letitgo/public/input/sketch.js
+++ Letitgo/public/input/sketch.js
@@ -14,12 +14,6 @@ let users = {};
 // Test word, just to make sure the connection between input and output is working
 let test = "Is it?";
 
-// Streeming bears
-let bears;
-
-// Video
-let video;
-
 function preload() {
   myFont = loadFont('DIN BLACK.ttf');
   bears = loadImage("./bears.jpeg");
@@ -46,33 +40,68 @@ function setup() {
     delete users[id];
   });
 
-  // Canvas to bolb
-  let canvas = document.getElementsByTagName('canvas')[0];
-  canvas.toBlob(function(file) {
-    console.log(file);
-    let stream = ss.createStream();
-    console.log(file);
+}
 
-    // upload a file to the server.
-    ss(socket).emit('file', stream, {
-      name: 'blob.png'
-    });
-    ss.createBlobReadStream(file).pipe(stream);
-  });
+//Picmode
+function Picmode() {
+  createCanvas(windowWidth, windowHeight);
+  firstbutton.hide();
 
-  //Picmode
-  function Picmode() {
-    createCanvas(390, 240);
-    capture = createCapture(VIDEO);
-    //capture.size(320, 240);
-    firstbutton.hide();
-    console.log("Pic mode");
+  // Button that takes you from Instructions to Picmode
+  buttonforpic = createButton("I'm ready");
+  buttonforpic.mousePressed(takeablob);
+  buttonforpic.position(100, 600);
+
+  //Just web camera stuff
+  navigator.getUserMedia = navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
+
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia({
+        audio: false,
+        video: {
+          width: 1280,
+          height: 720
+        }
+      },
+
+      handleNewVideoFrame,
+      function(err) {
+        console.log("The following error occurred: " + err.name);
+      }
+    );
+  } else {
+    console.log("getUserMedia not supported");
   }
 
+  // Canvas to bolb
+  function takeablob() {
+    let canvas = document.getElementsByTagName('canvas')[0];
+    canvas.toBlob(function(file) {
+      console.log(file);
+      let stream = ss.createStream();
+      console.log(file);
+
+      // upload a file to the server.
+      ss(socket).emit('file', stream, {
+        name: 'blob.png'
+      });
+      ss.createBlobReadStream(file).pipe(stream);
+    });
+  }
+
+  function handleNewVideoFrame(frame) {
+    console.log(frame)
+    var video = document.querySelector('video');
+    video.srcObject = frame;
+    video.onloadedmetadata = function(e) {
+      video.play();
+    };
+  }
 }
+
 
 function draw() {
   socket.emit('info', test);
-  // See the bears
-  //image(bears, 0, 0);
 }
