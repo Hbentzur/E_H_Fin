6a5b9975ad20a36493810a5cf0df0a566889744c
Hbentzur <31830472+Hbentzur@users.noreply.github.com>
2018-04-24 16:46:19 -0400

    End of day, 4.23

--- Letitgo/public/input/sketch.js
+++ Letitgo/public/input/sketch.js
@@ -14,13 +14,28 @@ let users = {};
 // Test word, just to make sure the connection between input and output is working
 let test = "Is it?";
 
+let myCanvas;
+let myContext;
+let myVideo;
+
+let frame;
+let video;
+
+let firstbutton;
+let buttonforpic;
+
 function preload() {
   myFont = loadFont('DIN BLACK.ttf');
   bears = loadImage("./bears.jpeg");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+
+  myCanvas = createCanvas(windowWidth, windowHeight);
+  myContext = myCanvas.canvas.getContext('2d');
+  myVideo = document.getElementById('video');
+
+  // video.addEventListener();
 
   //First screen
   background(40, 40, 40);
@@ -28,7 +43,7 @@ function setup() {
   textFont(myFont);
   textAlign(CENTER);
   fill(250, 200, 200);
-  text("Take a picture \nof something \nyou don't need \nanymore.", windowWidth / 2, windowHeight / 2 - 30);
+  text("Take a picture \nof something \nyou don't need \nanymore.", windowWidth / 2, windowHeight / 2);
 
   // Button that takes you from Instructions to Picmode
   firstbutton = createButton("I'm ready");
@@ -44,7 +59,7 @@ function setup() {
 
 //Picmode
 function Picmode() {
-  createCanvas(windowWidth, windowHeight);
+  // createCanvas(600, 600);
   firstbutton.hide();
 
   // Button that takes you from Instructions to Picmode
@@ -75,14 +90,22 @@ function Picmode() {
     console.log("getUserMedia not supported");
   }
 
-  // Canvas to bolb
+  function handleNewVideoFrame(frame) {
+    var video = document.querySelector('video');
+    video.srcObject = frame;
+    video.onloadedmetadata = function(e) {
+      video.play();
+      console.log(video);
+    };
+  }
+
+  // Canvas to blob
   function takeablob() {
     let canvas = document.getElementsByTagName('canvas')[0];
+    // let canvas = document.getElementsByTagName('video')[0];
     canvas.toBlob(function(file) {
       console.log(file);
       let stream = ss.createStream();
-      console.log(file);
-
       // upload a file to the server.
       ss(socket).emit('file', stream, {
         name: 'blob.png'
@@ -91,14 +114,14 @@ function Picmode() {
     });
   }
 
-  function handleNewVideoFrame(frame) {
-    console.log(frame)
-    var video = document.querySelector('video');
-    video.srcObject = frame;
-    video.onloadedmetadata = function(e) {
-      video.play();
-    };
-  }
+  //image(frame, 0, 0, 240, 340);
+  //Select video element in index.html
+  // let userVideo = document.getElementsByTagName('video');
+  // image(userVideo, 0, 0, 240, 340);
+  //video.html();
+  //image(userVideo)
+  //Display it to the p5 canvas
+
 }
 
 
